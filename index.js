fc05c06ab6f28843421fb433669ef6e7cb5c4a2e
darshanbajania <darshanbajania1999@gmail.com>
2022-12-23 18:19:23 +0530

    added recaptcha

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.all('/', async (req, resp) => {
+
+    let gToken = req.body.gToken
+    let formData = new FormData;
+    // formData.append('secret', '6LfvcJ8jAAAAAGIDiRhcdKo5d3hGDmJ_s7RfhwVZ')
+    formData.append('secret', process.env.gCaptchaKey)
+    formData.append('response', gToken)
+    fetch('https://www.google.com/recaptcha/api/siteverify', {
+        method: 'POST',
+        body: formData
+    }).then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            resp.send(data)
+        });
+
+
+
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3001)
