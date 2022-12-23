a1ce102b7c1958bdf8fa90081d2b9320d39c39e8
darshanbajania <darshanbajania1999@gmail.com>
2022-12-23 20:10:37 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
+
 app.use(
     express.urlencoded({
         extended: true
     })
 )
-
+app.use(cors())
 app.use(express.json())
 
 app.all('/', async (req, resp) => {
