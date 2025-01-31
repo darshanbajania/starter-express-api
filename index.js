93da83a2bbfb7525308ccc1fd5a1889986d729ba
darshanbajania <darshanbajania1999@gmail.com>
2025-01-31 21:24:00 +0530

    fix: env not readable

--- index.js
+++ index.js
@@ -1,64 +1,57 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+let dotenv = require("dotenv").config();
+const app = express();
 
 const { Configuration, OpenAIApi } = require("openai");
-const cors = require('cors')
+const cors = require("cors");
 
-const router = express.Router()
+const router = express.Router();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-
-
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-
 app.use(
-    express.urlencoded({
-        extended: true
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  let data;
+  res.send("Hello World!" + process.env.OPENAI_API_KEY);
+});
+
+app.post("/", async (req, resp) => {
+  let gToken = req.body.gToken;
+  let formData = new FormData();
+  console.log("gtoken", gToken);
+  formData.append("secret", process.env.gCaptchaKey);
+  formData.append("response", gToken);
+  try {
+    fetch("https://www.google.com/recaptcha/api/siteverify", {
+      method: "POST",
+      body: formData,
     })
-)
-app.use(cors())
-app.use(express.json())
-
-
-
-app.post('/', async (req, resp) => {
-
-    let gToken = req.body.gToken
-    let formData = new FormData;
-    console.log('gtoken', gToken)
-    formData.append('secret', process.env.gCaptchaKey)
-    formData.append('response', gToken)
-    try {
-
-        fetch('https://www.google.com/recaptcha/api/siteverify', {
-            method: 'POST',
-            body: formData
-        }).then((response) => response.json())
-            .then((data) => {
-                console.log('rechaptcha data', data)
-                resp.send(data)
-            });
-    } catch (error) {
-        console.log('gtoken error', error)
-        resp.send({
-            success: false,
-            message: 'captcha error'
-        })
-    }
-
-
-
-
-
-
-})
-app.post('/solve', async (req, res) => {
-    const queryText = req.body.queryText
-    const promptText = `You answer mathematics problems like below:
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("rechaptcha data", data);
+        resp.send(data);
+      });
+  } catch (error) {
+    console.log("gtoken error", error);
+    resp.send({
+      success: false,
+      message: "captcha error",
+    });
+  }
+});
+app.post("/solve", async (req, res) => {
+  const queryText = req.body.queryText;
+  const promptText = `You answer mathematics problems like below:
 
 Q: sum of 5 and 4
 A:9
@@ -67,17 +60,17 @@ Q: What is the solution of (255+5)/2
 A:130
 
 Q: ${queryText}
-A:`
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: promptText,
-        temperature: 0,
-        max_tokens: 7,
-    });
+A:`;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: promptText,
+    temperature: 0,
+    max_tokens: 7,
+  });
 
-    const data = response.data
-    // const data = JSON.stringify(queryText)
-    console.log('solved', queryText)
-    res.send(data)
-})
-app.listen(process.env.PORT || 3001)
+  const data = response.data;
+  // const data = JSON.stringify(queryText)
+  console.log("solved", queryText);
+  res.send(data);
+});
+app.listen(process.env.PORT || 3001);
